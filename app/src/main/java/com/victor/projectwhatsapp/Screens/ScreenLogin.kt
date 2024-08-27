e37e13bd334f3898be4a7590d101030b8996c493
Victor-Valedev <victorvaledev@gmail.com>
2024-08-26 22:37:23 -0300

    feat: Created Screen Login

---
+++ app/src/main/java/com/victor/projectwhatsapp/Screens/ScreenLogin.kt
@@ -0,0 +1,15 @@
+package com.victor.projectwhatsapp.Screens
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.victor.projectwhatsapp.R
+
+class ScreenLogin : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_screen_login)
+    }
+}
