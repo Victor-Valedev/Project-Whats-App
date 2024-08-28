d36beccb9f1f41febd27ccf31974d6e6d01578ad
Victor-Valedev <victorvaledev@gmail.com>
2024-08-27 23:22:18 -0300

    feat: Created switch button to change the theme on the login screen, to dark or light

--- app/src/main/java/com/victor/projectwhatsapp/Screens/ScreenLogin.kt
+++ app/src/main/java/com/victor/projectwhatsapp/Screens/ScreenLogin.kt
@@ -3,13 +3,30 @@ package com.victor.projectwhatsapp.Screens
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.victor.projectwhatsapp.R
+import com.victor.projectwhatsapp.databinding.ActivityScreenLoginBinding
 
 class ScreenLogin : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityScreenLoginBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_screen_login)
+        setContentView(binding.root)
+
+        //Mudando tema do App
+        binding.buttonTema.setOnCheckedChangeListener{ _, isChecked ->
+            if(isChecked){
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            }else{
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            }
+        }
+
     }
 }
