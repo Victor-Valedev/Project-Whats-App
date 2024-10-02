bf6e1a6ff5dfe25f2ad172003f506f06a8df2424
Victor-Valedev <victorvaledev@gmail.com>
2024-10-01 21:44:51 -0300

    feat: Created function to update user data directly in firestore, and media files in firestorage, also created function to update user name and profile photo.

--- app/src/main/java/com/victor/projectwhatsapp/MainActivity.kt
+++ app/src/main/java/com/victor/projectwhatsapp/MainActivity.kt
@@ -1,13 +1,10 @@
 package com.victor.projectwhatsapp
 
-import android.Manifest
 import android.content.Intent
-import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
--- app/src/main/java/com/victor/projectwhatsapp/Screens/ProfileUserActivity.kt
+++ app/src/main/java/com/victor/projectwhatsapp/Screens/ProfileUserActivity.kt
@@ -2,6 +2,7 @@ package com.victor.projectwhatsapp.Screens
 
 import android.Manifest
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
@@ -11,8 +12,12 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.FirebaseStorage
 import com.victor.projectwhatsapp.R
 import com.victor.projectwhatsapp.databinding.ActivityProfileUserBinding
+import com.victor.projectwhatsapp.utils.showMessage
 
 class ProfileUserActivity : AppCompatActivity() {
 
@@ -20,6 +25,18 @@ class ProfileUserActivity : AppCompatActivity() {
 
     private var isPermissionCamera = false
     private var isPermissionGallery = false
+
+    private val firebaseAuth by lazy {
+        FirebaseAuth.getInstance()
+    }
+
+    private val storage by lazy {
+        FirebaseStorage.getInstance()
+    }
+
+    private val firestore by lazy {
+        FirebaseFirestore.getInstance()
+    }
 
     // Registrar o resultado da solicitação de permissões fora de métodos de instância
     @RequiresApi(Build.VERSION_CODES.TIRAMISU)
@@ -30,6 +47,18 @@ class ProfileUserActivity : AppCompatActivity() {
         isPermissionGallery = permissions[Manifest.permission.READ_MEDIA_IMAGES] ?: isPermissionGallery
     }
 
+    private val managerGallery = registerForActivityResult(
+        ActivityResultContracts.GetContent()
+    ) { uri ->
+        if (uri != null) {
+            binding.imageProfile.setImageURI(uri)
+            uploadImageStorage(uri)
+        } else {
+            showMessage("Nenhuma imagem foi selecionada")
+        }
+    }
+
+
     @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,8 +66,97 @@ class ProfileUserActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         initializeToolBarProfile()
+        initializaEventsClick()
 
+
+    }
+
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
+    private fun initializaEventsClick() {
         binding.fbAddPhotoGallery.setOnClickListener {
+            requestPermissionsApp()
+            initializeEventsOnclick()
+        }
+
+        binding.btnAtualizarPerfil.setOnClickListener {
+
+            val nameUser = binding.editTextNameProfile.text.toString()
+            if(nameUser.isNotEmpty()){
+
+                val idUser = firebaseAuth.currentUser?.uid
+                if(idUser != null){
+                    val dataUser = mapOf(
+                        "nome" to nameUser
+                    )
+                    updateDataProfile(idUser,dataUser)
+                }
+
+
+
+            }else{
+              showMessage("Preencha um nome para atualizar")
+            }
+
+        }
+
+    }
+
+    private fun uploadImageStorage(uri: Uri) {
+
+        //images -> users -> idUser -> profile.jpg
+
+        val idUser = firebaseAuth.currentUser?.uid
+        if (idUser != null) {
+            storage
+                .getReference("images")
+                .child("users")
+                .child(idUser)
+                .child("profile.jpg")
+                .putFile(uri)
+                .addOnSuccessListener { task ->
+                    showMessage("Sucesso ao fazer upload da imagem")
+
+                    task.metadata
+                        ?.reference
+                        ?.downloadUrl
+                        ?.addOnSuccessListener { url ->
+
+                            val dataUser = mapOf(
+                                "foto" to url.toString()
+                            )
+                            
+                            updateDataProfile(idUser,dataUser)
+                            
+                        }
+
+                }.addOnFailureListener {
+                    showMessage("Erro ao fazer upload da imagem")
+                }
+        }
+    }
+
+    private fun updateDataProfile(idUser: String, dataUser: Map<String, String>) {
+
+            firestore
+                .collection("users")
+                .document(idUser)
+                .update(dataUser)
+                .addOnSuccessListener {
+                    showMessage("Sucesso ao atualizar perfil")
+                }
+                .addOnFailureListener {
+                    showMessage("Erro ao atualizar perfil")
+                }
+
+    }
+
+
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
+    private fun initializeEventsOnclick() {
+        if (isPermissionGallery) {
+            managerGallery.launch("image/*")
+        } else {
+            showMessage("Permita o acesso a galeria")
             requestPermissionsApp()
         }
     }
